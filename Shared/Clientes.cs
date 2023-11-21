3739a23ca6b83a6755edaeaa8fe195460f2497b4
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2023-11-21 19:08:23 -0400

    Clase Clientes creada

---
+++ Shared/Clientes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2MABB.Shared
+{
+    internal class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        [Required(ErrorMessage = "Debe ingresar el nombre")]
+        [MinLength(2, ErrorMessage = "El nombre debe tener al menos 2 caracteres")]
+        [RegularExpression(@"^[a-zA-ZáéíóúüÁÉÍÓÚÜ\s]+$", ErrorMessage = "Solo se permiten letras y espacios")]
+        public string? Nombres { get; set; }
+    }
+}
