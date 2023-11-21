270e2a147b7766c9a6e52d47bfe11619db37a1dd
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2023-11-21 19:09:20 -0400

    Clase Ventas Creada

---
+++ Shared/Ventas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2MABB.Shared
+{
+    internal class Ventas
+    {
+        [Key]
+        public int VentaId { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public int ClienteId { get; set; }
+        public double Monto { get; set; }
+        public double Balance { get; set; }
+        public string? Observaciones { get; set; }
+
+        public ICollection<VentasDetalles> ventasDetalles { get; set; } = new List<VentasDetalles>();
+    }
+}
