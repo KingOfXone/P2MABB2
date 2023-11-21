fdd67ad7ed015c4a4949c8853e551d992c9e87f4
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2023-11-21 18:50:50 -0400

    Desde 0

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddDbContext<Context>(options => options.UseSqlite(ConStr));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
