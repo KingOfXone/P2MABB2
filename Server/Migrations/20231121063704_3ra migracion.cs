b096d1d4772bfb0f2022fc8f3bc7acb272a3e47e
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2023-11-23 21:48:46 -0400

    .

---
+++ Server/Migrations/20231121063704_3ra migracion.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace P2MABB.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class _3ramigracion_Cambios_en_clases_Cobrado_y_eldetail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Server/Migrations/20231121070109_5ra migracion.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace P2MABB.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class _5ramigracion_agregando_a_el_detail_un_Booleano : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
