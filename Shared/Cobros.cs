946c6e92bed6c72adc678244a6a1758346e4b9ec
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2023-11-23 21:17:22 -0400

    Clase Cobros

---
+++ Shared/Cobros.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2MABB.Shared
+{
+	public class Cobros
+	{
+		[Key]
+		public int CobroId { get; set; }
+		public string? Observaciones { get; set; } = string.Empty;
+
+		public DateTime Fecha { get; set; } = DateTime.Now;
+
+		[ForeignKey("CobradoDetailId")]
+		public ICollection<CobrosDetalle> CobradoDetail { get; set; } = new List<CobrosDetalle>();
+	}
+}
